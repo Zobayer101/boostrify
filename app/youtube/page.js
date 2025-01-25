4bc139d26749d987f09f9d2fe7fcf4521bd76f43
Zobayer <mdzobayerhossen520@gmail.com>
2025-01-25 19:31:59 +0600

    new feture addd

--- app/youtube/page.js
+++ app/youtube/page.js
@@ -1,10 +1,20 @@
 import Navbar from "../Components/Navbar";
 import "../design/Navbar.css";
+import "../design/Youtube.css";
+import Youtubes from "../Icon/youtube-Photoroom.png";
+import Image from "next/image";
+import BostChannel from "../Icon/BostChannel.png";
 const Youtube = () => {
   return (
     <div className="youtubePage">
       <Navbar />
-      
+      <div className="View">
+        <Image src={Youtubes} alt="youtube" width={70} />
+        <div className="hader">grow your channel with real traffic !</div>
+      </div>
+      <div className="graphics">
+        <Image src={BostChannel} alt="Bostchannel" width={500} />
+      </div>
     </div>
   );
 };
