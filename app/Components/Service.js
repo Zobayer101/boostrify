50f4d4c52390213922fb94b4669b270ab52e697f
Zobayer <mdzobayerhossen520@gmail.com>
2025-01-27 23:18:51 +0600

    Add feture

---
+++ app/Components/Service.js
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import LikeYoutube from "../Icon/likeyoutube.png";
+import Follows from "../Icon/ReadFollow.png";
+import subscrib from "../Icon/subscrib.png";
+import youtubeShear from "../Icon/shareyoutube.png";
+import YoutubeComment from "../Icon/commentyoutube.png";
+
+const Services = ({ props }) => {
+  return (
+    <div className="ServiceSection">
+      <div className="OneCollum">
+        <div className="CoverOne">
+          <div className="Imgtexts">
+            <Image src={LikeYoutube} alt="like now" width={100} />
+
+            <p>Price / 1: 0.01 $</p>
+          </div>
+          <div className="InputBox">
+            <input type="number" />
+            <div className="total">10 $</div>
+          </div>
+        </div>
+        <div className="Covertow">
+          <div className="Imgtexts">
+            <Image src={YoutubeComment} alt="like now" width={100} />
+            <p>Price / 1: 0.01 $</p>
+          </div>
+          <div className="InputBox">
+            <input type="number" />
+            <div className="total">10 $</div>
+          </div>
+        </div>
+      </div>
+      <div className="towCollum">
+        <div className="Coverx">
+          <div className="Imgtexts">
+            <Image
+              src={props == "follow" ? Follows : subscrib}
+              alt="like now"
+              width={100}
+            />
+            <p>Price / 1: 0.05 $</p>
+          </div>
+          <div className="InputBox">
+            <input type="number" />
+            <div className="total">10 $</div>
+          </div>
+        </div>
+        <div className="Covery">
+          <div className="Imgtexts">
+            <Image src={youtubeShear} alt="like now" width={100} />
+            <p>Price / 1: 0.01 $</p>
+          </div>
+          <div className="InputBox">
+            <input type="number" />
+            <div className="total">10 $</div>
+          </div>
+        </div>
+      </div>
+      <div className="NextCollum">
+        <div className="Textaction">Total Price 25$</div>
+        <div className="saveBtn">Free Now</div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
--- app/youtube/page.js
+++ app/youtube/page.js
@@ -3,18 +3,34 @@ import "../design/Navbar.css";
 import "../design/Youtube.css";
 import Youtubes from "../Icon/youtube-Photoroom.png";
 import Image from "next/image";
+
 import BostChannel from "../Icon/BostChannel.png";
+import Services from "../Components/Service";
 const Youtube = () => {
   return (
     <div className="youtubePage">
       <Navbar />
       <div className="View">
         <Image src={Youtubes} alt="youtube" width={70} />
-        <div className="hader">grow your channel with real traffic !</div>
+        <div className="hader">grow your channel with real traffic</div>
       </div>
       <div className="graphics">
-        <Image src={BostChannel} alt="Bostchannel" width={500} />
+        <div className="CoverGraphics">
+          <Image src={BostChannel} alt="Bostchannel" width={500} />
+        </div>
+        <div className="AboutText">
+          <p className="paragraph">
+            Boostrify is one of the best social media boosting platforms
+            designed to help you grow your YouTube channel with real traffic. By
+            completing a few simple tasks, you can increase your reach, gain
+            authentic engagement, and watch your channel thrive. It's fast, easy
+            to use, and offers real results to help content creators succeed in
+            today’s competitive digital world. Boost your presence with
+            confidence and watch your growth skyrocket!
+          </p>
+        </div>
       </div>
+      <Services />
     </div>
   );
 };
